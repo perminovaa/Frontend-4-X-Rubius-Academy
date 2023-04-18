5bd4222655cc0d8ef7078e732dd956be4e10f8c7
Anastasiia Perminova <a.perminova.a@yandex.ru>
2023-04-18 11:54:26 +0700

    Задание Sprint4 выполнено

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+
+// Галерея
+$(document).ready(function(){
+    $('.gallery').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+});
